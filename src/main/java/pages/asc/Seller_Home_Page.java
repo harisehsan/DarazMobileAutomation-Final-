04ac828279c6b5cab0f11a9b319da6feab3abb1b
admin.son.ton <son.tonchi@lazada.com>
2018-08-13 19:26:04 +0800

    refactor

--- src/main/java/pages/asc/Seller_Home_Page.java
+++ src/main/java/pages/asc/Seller_Home_Page.java
@@ -10,8 +10,6 @@ public class Seller_Home_Page extends PageObject {
     private static final String page_url = Global.config.getString("asc.home_url");
 
     @FindBy(css = ".la-header-nav-user-content .username") private WebElement displayed_user_nav;
-//    @FindBy(xpath = "//span[text()='Orders']") private WebElement displayed_user_nav;
-//    @FindBy(xpath = ".la-header-nav-user-content .username") private WebElement displayed_user_nav;
 
     public String currentSellerName(){
         waitUntilPageReady();
