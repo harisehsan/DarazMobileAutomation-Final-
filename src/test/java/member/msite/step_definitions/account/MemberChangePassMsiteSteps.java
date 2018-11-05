a1a00534d47b51e8ea4f7c26e182f64ed64b8ae6
van.vo <van.vo@lazada.com>
2018-11-05 14:36:26 +0700

    Van commit task 17480573 : Msite : ChangePassword and login by new password

---
+++ src/test/java/member/msite/step_definitions/account/MemberChangePassMsiteSteps.java
@@ -0,0 +1,39 @@
+package member.msite.step_definitions.account;
+
+import base.BaseSteps;
+import cucumber.api.java.en.*;
+import global.Global;
+import member.msite.pages.account.Member_AccountSetting_Msite_Page;
+import member.msite.pages.account.Member_ChangePass_Msite_Page;
+
+public class MemberChangePassMsiteSteps extends BaseSteps {
+
+    @And("^I click to change password button")
+    public void clickChangePassBtn() throws Throwable{
+        on(Member_AccountSetting_Msite_Page.class).changePassWord();
+    }
+
+
+    @And("^I go to change pass page on Msite")
+    public void accessChangePass() throws Throwable {
+        visit(Member_ChangePass_Msite_Page.class);
+    }
+
+    @And("^I input all information to change pass")
+    public void changePassProcess() throws Throwable {
+        String old_pass = Global.config.getString("member.account.pass");
+        String new_pass = Global.config.getString("member.account.new_pass");
+        on(Member_ChangePass_Msite_Page.class).setNewPassWord(old_pass,new_pass,new_pass);
+    }
+
+
+    @And("^I click save password button")
+    public void clickSaveBtn() throws Throwable {
+        on(Member_ChangePass_Msite_Page.class).saveButton();
+    }
+
+    @And("^I click got it button on popup")
+    public void gotItButton() throws Throwable {
+        on(Member_ChangePass_Msite_Page.class).gotItButton();
+    }
+}
