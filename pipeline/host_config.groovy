b1571bab4ad7b5bb061b02acc3a3d13badc3f87c
admin.son.ton <son.tonchi@lazada.com>
2018-11-19 20:43:54 +0700

    add host_config

--- pipeline/host_config.groovy
+++ pipeline/host_config.groovy
@@ -1,5 +1,5 @@
 def setHost(String venture_env){
-    if(venture_env.equalsIgnoreCase("staging")){
+    if(venture_env.contains("staging")){
         def output=readFile(pwd()+"/host/staging_host").trim()
         sh "echo \"${output}\" >> /etc/hosts"
     }
