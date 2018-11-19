bedf24523f878b45c5d262b2c3b8f76b12966b66
admin.son.ton <son.tonchi@lazada.com>
2018-11-19 19:27:38 +0700

    update script

--- pipeline/BuildLive.groovy
+++ pipeline/BuildLive.groovy
@@ -2,9 +2,9 @@ def build(String Module, String Theme,String Tags,String Venture_Env){
     if (isUnix()) {
         String venture = Venture_Env.split("\\.")[0]
         String env = Venture_Env.split("\\.")[1]
-        String excludedTags = "'not @no_${env}','not @no_${venture}','not no_${venture}_${env}'"
-        String resultTags = excludedTags +","+Tags
-        String cucumberOpt = "\"src/test/java/${Module}/${Theme}/features --tags ${resultTags} --glue ${Module}.${Theme}.step_definitions --glue _base.${Theme}_steps --glue _base.api_steps\""
+        String excludedTags = "--tags 'not @no_${env}',--tags 'not @no_${venture}',--tags 'not @no_${venture}_${env}'"
+        String resultTags = excludedTags +",--tags "+Tags
+        String cucumberOpt = "\"src/test/java/${Module}/${Theme}/features ${resultTags} --glue ${Module}.${Theme}.step_definitions --glue _base.${Theme}_steps --glue _base.api_steps\""
         try {
             sh "mvn clean test -Dcucumber.options=${cucumberOpt} -Denv=\"${Venture}\" -Dtheme=\"${Theme}\""
             currentBuild.result = 'SUCCESS'
