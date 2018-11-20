b1d36fc2a96a4b1890eb9490c825b70c37299f1e
admin.son.ton <son.tonchi@lazada.com>
2018-11-20 15:53:17 +0700

    add api step

--- src/test/java/_base/api_steps/MemberApiSteps.java
+++ src/test/java/_base/api_steps/MemberApiSteps.java
@@ -4,6 +4,7 @@ import base.BaseSteps;
 import com.google.gson.JsonObject;
 import cucumber.api.java.en.And;
 import global.Global;
+import helper.RandomeHelper;
 import helper.XhrHelper;
 
 public class MemberApiSteps extends BaseSteps {
@@ -18,6 +19,22 @@ public class MemberApiSteps extends BaseSteps {
         JsonObject response = XhrHelper.executeXhrRequest("member_login.js",args);
         if(!String.valueOf(response.get("success")).equalsIgnoreCase("true")){
             throw new RuntimeException(String.format("Login with credential %s/%s fail . Response from server: %s",email,pass,String.valueOf(response)));
+        }
+        Global.browser.refresh();
+    }
+
+    @And("^I sign up by api")
+    public void signUpByApi(){
+        String csrfToken = Global.browser.getCookiesAsMap().get("_tb_token_");
+        String randomEmail = "LAZADATEST_1111_" + RandomeHelper.generateEmail()+ "@hotmail.com";
+        Global.map.put("email_random",randomEmail);
+        String pass = Global.config.getString("member.account.pass");
+        String name = Global.config.getString("member.account.name");
+        String apiUrl = Global.config.getString("member.url")+"/user/api/register";
+        String [] args = {apiUrl,randomEmail,pass,name,csrfToken};
+        JsonObject response = XhrHelper.executeXhrRequest("member_signup.js",args);
+        if(!String.valueOf(response.get("success")).equalsIgnoreCase("true")){
+            throw new RuntimeException(String.format("Sign up with credential %s/%s fail . Response from server: %s",randomEmail,pass,String.valueOf(response)));
         }
         Global.browser.refresh();
     }
---
+++ src/test/resources/xhr_script/member_signup.js
@@ -0,0 +1,33 @@
+
+
+var apiUrl=arguments[0];
+var email=arguments[1];
+var password=arguments[2];
+var name=arguments[3];
+var csrfToken=arguments[4];
+
+
+
+var data = JSON.stringify({
+  "lzdAppVersion": "1.0",
+  "email": email,
+  "password": password,
+  "name": name,
+  "enableNewsletter": true
+});
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+var callback = arguments[arguments.length - 1];
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    callback(xhr.responseText);
+  }
+});
+
+xhr.open("POST", apiUrl);
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("x-csrf-token", csrfToken);
+xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.send(data);
