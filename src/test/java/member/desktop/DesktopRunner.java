b7000f2b4774e0b63e412203162a762ab33deec0
admin.son.ton <son.tonchi@lazada.com>
2018-11-22 11:03:20 +0700

    fix conflict

--- src/test/java/member/desktop/DesktopRunner.java
+++ src/test/java/member/desktop/DesktopRunner.java
@@ -10,6 +10,6 @@ import cucumber.api.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/test/java/member/desktop/features"},
-        tags = {"@17916529"},
+        tags = {"@Loginbyemail"},
         glue = {"member.desktop.step_definitions"})
 public class DesktopRunner extends TestRunner {}
