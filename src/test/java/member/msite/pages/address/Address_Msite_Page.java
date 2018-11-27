3a5f6ffc6a0fd974fa37be74365085374aef37f0
van.vo <van.vo@lazada.com>
2018-11-27 17:12:52 +0700

    Van commit task 17480541 : Msite :  Edit address information

--- src/test/java/member/msite/pages/address/Address_Msite_Page.java
+++ src/test/java/member/msite/pages/address/Address_Msite_Page.java
@@ -12,26 +12,27 @@ public class Address_Msite_Page extends PageObject {
 
     public static String page_url = Global.config.getString("member.url") + "/address";
 
-    @FindBy(css = "#container > div > div.mod-tabs > ul > li.mod-tabs-item.active") private WebElement shippingAddd;
-    @FindBy(css = "#container > div > div.mod-tabs > ul > li:nth-child(2)") private WebElement billingAdd;
-    @FindBy(css = "#container > div > div.address-list > div.address-list-footer > button") private WebElement addNewAddressBtn;
-    @FindBy(css = ".mod-input-name input") private WebElement inputName;
-    @FindBy(css = ".mod-input-phone input") private WebElement inputPhone;
-    @FindBy(css = ".mod-select-location-tree-1") private WebElement locationTree1;
-    @FindBy(css = "#container > div > div > div > div.mod-address-form-inputs > div.mod-select.mod-select-location-tree-2") private WebElement locationTree2;
-    @FindBy(css = "#container > div > div > div > div.mod-address-form-inputs > div.mod-select.mod-select-location-tree-3") private WebElement locationTree3;
-    @FindBy(xpath = "//*[@id=\"container\"]/div/div/div/div[1]/div[3]/select/option[2]") private List<WebElement> selectLocation1;
-    @FindBy(xpath = "//*[@id=\"container\"]/div/div/div/div[1]/div[4]/select/option[2]") private List<WebElement> selectLocation2;
-    @FindBy(xpath = "//*[@id=\"container\"]/div/div/div/div[1]/div[5]/select/option[2]") private List<WebElement> selectLocation3;
-    @FindBy(css = ".mod-input-detailAddress input") private WebElement addDetail;
-    @FindBy(className = "mod-radio") private WebElement shippingDefault;
-    @FindBy(css = ".mod-address-form-yatra-btn") private WebElement saveAddressBtn;
-    @FindBy(css = ".address-list-body > div") private List<WebElement> addressList;
+    @FindBy(css = "div.address-list-footer > button") private WebElement addNewAddress_btn;
+    @FindBy(css = "div.mod-input-name > input") private WebElement addressName_txtField;
+    @FindBy(css = "div.mod-input-phone > input") private WebElement addressPhone_txtField;
+    @FindBy(css = "div.mod-select-location-tree-1") private WebElement locationTree1_listBtn;
+    @FindBy(css = "div.mod-select.mod-select-location-tree-2") private WebElement locationTree2_listBtn;
+    @FindBy(css = "div.mod-select.mod-select-location-tree-3") private WebElement locationTree3_listBtn;
+    @FindBy(css = "div.mod-select-location-tree-1 > select > option:nth-child(2)") private List<WebElement> selectLocation1_drpDownList;
+    @FindBy(css = "div.mod-select-location-tree-2 > select > option:nth-child(2)") private List<WebElement> selectLocation2_drpDownList;
+    @FindBy(css = "div.mod-select-location-tree-3 > select > option:nth-child(2)") private List<WebElement> selectLocation3_drpDownList;
+    @FindBy(css = ".mod-input-detailAddress input") private WebElement addressDetail_txtField;
+    @FindBy(css = ".mod-address-form-yatra-btn") private WebElement saveAddress_btn;
+    @FindBy(css = ".address-list-body > div") private List<WebElement> address_list;
+    @FindBy(css = "div > i.address-list-item-edit") private WebElement editAddress_btn;
+    @FindBy(css = "div.mod-input-name > div") private WebElement removeAddressName_btn;
+    @FindBy(css = "div.mod-input-phone > div") private WebElement removeAddressPhone_btn;
+    @FindBy(css = "div.mod-address-item-user") private WebElement addressNameUser_lbl;
 
     public void clickNewAddress() {
         waitUntilPageReady();
-        waitUntilVisible(addNewAddressBtn);
-        this.addNewAddressBtn.click();
+        waitUntilVisible(addNewAddress_btn);
+        this.addNewAddress_btn.click();
     }
 
     public void selectTree(List<WebElement> selectTree) {
@@ -41,34 +42,58 @@ public class Address_Msite_Page extends PageObject {
         }
     }
 
-    public void inputAddressDetail(String addName, String addPhone, String addressDetail) {
+    public void inputAddressDetail(String addName, String addPhone) {
         waitUntilPageReady();
-        waitUntilVisible(locationTree1);
-        this.inputName.sendKeys(addName);
-        this.inputPhone.sendKeys(addPhone);
+        waitUntilVisible(locationTree1_listBtn);
+        this.addressName_txtField.sendKeys(addName);
+        this.addressPhone_txtField.sendKeys(addPhone);
         waitUntilClickable(By.cssSelector(".mod-select-location-tree-1"));
-        this.locationTree1.click();
-        selectTree(selectLocation1);
+        this.locationTree1_listBtn.click();
+        selectTree(selectLocation1_drpDownList);
         waitUntilInvisibilityOf(By.id("__react_loading_show__"));
-        this.locationTree2.click();
+        this.locationTree2_listBtn.click();
         waitUntilInvisibilityOf(By.id("__react_loading_show__"));
-        selectTree(selectLocation2);
+        selectTree(selectLocation2_drpDownList);
         waitUntilInvisibilityOf(By.id("__react_loading_show__"));
-        this.locationTree3.click();
+        this.locationTree3_listBtn.click();
         waitUntilInvisibilityOf(By.id("__react_loading_show__"));
-        selectTree(selectLocation3);
+        selectTree(selectLocation3_drpDownList);
         waitUntilInvisibilityOf(By.id("__react_loading_show__"));
-        this.addDetail.sendKeys(addressDetail);
+    }
+
+    public void inputAddressContent(String address) {
+        waitUntilVisible(addressDetail_txtField);
+        this.addressDetail_txtField.sendKeys(address);
     }
 
     public void clickSaveBtn() {
         waitUntilPageReady();
-        waitUntilVisible(saveAddressBtn);
-        this.saveAddressBtn.click();
+        waitUntilVisible(saveAddress_btn);
+        this.saveAddress_btn.click();
+    }
+
+    public void editAddressInformation(String name, String phone) {
+        waitUntilPageReady();
+        waitUntilClickable(By.cssSelector("div > i.address-list-item-edit"));
+        this.editAddress_btn.click();
+        waitUntilClickable(By.cssSelector("div.mod-input-name > div"));
+        this.removeAddressName_btn.click();
+        this.removeAddressPhone_btn.click();
+        this.addressName_txtField.sendKeys(name);
+        this.addressPhone_txtField.sendKeys(phone);
+        this.saveAddress_btn.click();
+        waitUntilInvisibilityOf(By.id("__react_loading_show__"));
+    }
+
+    public String hasNewAddressName(){
+        waitUntilPageReady();
+        waitUntilVisible(addressNameUser_lbl);
+        return addressNameUser_lbl.getText();
     }
 
     public boolean hasAddress() {
         waitUntilPageReady();
-        return this.addressList.size() >= 1;
+        return this.address_list.size() >= 1;
     }
+
 }
