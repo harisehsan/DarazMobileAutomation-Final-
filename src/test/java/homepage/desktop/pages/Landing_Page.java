54ee5d91cf151bece3cc95d6ba9a807762ff2b90
tuong.nguyenngoc <tuong.nguyenngoc@lazada.com>
2018-11-27 11:43:09 +0700

    update small change on ID

--- src/test/java/homepage/desktop/pages/Landing_Page.java
+++ src/test/java/homepage/desktop/pages/Landing_Page.java
@@ -4,13 +4,15 @@ package homepage.desktop.pages;
 import base.*;
 import org.testng.Assert;
 
+import java.util.regex.Pattern;
+
 public class Landing_Page extends PageObject {
 
     boolean result;
 
     public void verifyAppLandingPage() {
         String url = currentUrl();
-        result = url.contains("app");
+        result = Pattern.compile(Pattern.quote("app"),Pattern.CASE_INSENSITIVE).matcher(url).find();
         Assert.assertTrue(result,"Fail cannot reach App Landing Page");
     }
 
