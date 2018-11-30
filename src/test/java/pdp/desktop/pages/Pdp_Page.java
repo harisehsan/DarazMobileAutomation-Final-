f43a9162930133255305b1a23cfcc45475a4ea3e
admin.son.ton <son.tonchi@lazada.com>
2018-11-30 16:12:07 +0700

    fix script

--- src/test/java/pdp/desktop/pages/Pdp_Page.java
+++ src/test/java/pdp/desktop/pages/Pdp_Page.java
@@ -108,11 +108,21 @@ public class Pdp_Page extends PageObject {
         waitUntilVisible(openAskQuestion_btn);
         waitUntilInvisibilityOf(By.cssSelector(".next-feedback-content"));
         openAskQuestion_btn.click();
+        CharSequence questionChar = question;
         System.out.println("dsadhjgasjdjksahdjkasjhk*******"+askQuestion_txtArea.getAttribute("value"));
         waitUntilVisible(askQuestion_txtArea);
-        askQuestion_txtArea.clear();
-        Global.browser.getJsExecutor().executeScript("arguments[0].value = '';", askQuestion_txtArea);
+
+//        Global.browser.getJsExecutor().executeScript("arguments[0].value = '';", askQuestion_txtArea);
         waitUntilVisible(askQuestion_txtArea);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        askQuestion_txtArea.clear();
+//        Global.browser.getJsExecutor().executeScript("arguments[0].value = '" + questionChar + "';", askQuestion_txtArea);
+//        Global.browser.getJsExecutor().executeScript("arguments[0].click();", askQuestion_btn);
+//        Global.browser.getJsExecutor().executeScript("arguments[0].value = 'arguments[1]';", askQuestion_txtArea,question);
         askQuestion_txtArea.sendKeys(question);
         askQuestion_btn.click();
     }
