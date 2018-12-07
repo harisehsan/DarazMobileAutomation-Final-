161a693cfe4ad72710e81d76a55dd2521f9f38e5
van.vo <van.vo@lazada.com>
2018-12-07 14:58:50 +0700

    Van commit task

--- src/test/java/member/desktop/DesktopRunner.java
+++ src/test/java/member/desktop/DesktopRunner.java
@@ -15,6 +15,6 @@ import java.util.List;
 
 @CucumberOptions(
         features = {"src/test/java/member/desktop/features"},
-        tags = {"@Address_PC"},
+        tags = {"@17807244"},
         glue = {"member.desktop.step_definitions"})
 public class DesktopRunner extends TestRunner {}
--- src/test/java/member/desktop/pages/address/Member_AddressPC_Page.java
+++ src/test/java/member/desktop/pages/address/Member_AddressPC_Page.java
@@ -21,7 +21,8 @@ public class Member_AddressPC_Page extends PageObject {
     @FindBy(css = ".mod-select-location-tree-3") private WebElement locationTree3_listBtn;
     @FindBy(css = ".mod-input-detailAddress > input") private WebElement address_txtField;
     @FindBy(css = ".mod-input-postCode > input[type='number']") private WebElement postCode_txtField;
-    @FindBy(xpath = "//button[contains(.,'SAVE')]") private WebElement save_btn;
+    @FindBy(xpath = "//button[contains(.,'SAVE')]") private WebElement savesg_btn;
+    @FindBy(css = "div.mod-address-form-action > button") private WebElement save_btn;
     @FindBy(className = "mod-address-book-default") private WebElement hasAddress_lbl;
     @FindBy(css = ".mod-input-detailAddress input") private WebElement detailAddress_txtField;
     @FindBy(css = ".next-table-cell") private WebElement editAddress_btn;
@@ -47,7 +48,8 @@ public class Member_AddressPC_Page extends PageObject {
     private By clearName_btn_by = By.cssSelector("div.mod-input-name > div");
     private By clearPhone_btn_by = By.cssSelector("div.mod-input-phone > div");
     private By selectTree_listItems_by = By.cssSelector(".next-menu-content > li");
-    private By save_btn_by = By.xpath("//button[contains(.,'SAVE')]");
+    private By savesg_btn_by = By.xpath("//button[contains(.,'SAVE')]");
+    private By save_btn_by = By.cssSelector("div.mod-address-form-action > button");
 
     public void addNewAddress() {
         waitUntilPageReady();
@@ -213,8 +215,8 @@ public class Member_AddressPC_Page extends PageObject {
         this.detailAddress_txtField.sendKeys(detailAdd);
         waitUntilInvisibilityOf(loadingicon_by);
 //        waitUntilClickable(save_btn_by);
-        this.save_btn.click();
-        waitUntilInvisibilityOf(save_btn_by);
+        this.savesg_btn.click();
+        waitUntilInvisibilityOf(savesg_btn_by);
     }
 
     public boolean hasNoAddressDeleted() {
