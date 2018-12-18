6968686735793b4fb0bc6554c54bcdcff0e5ed67
admin.son.ton <son.tonchi@lazada.com>
2018-12-18 14:46:39 +0700

    refactor search api

--- src/test/java/_base/api_helpers/buyer/BuyerSearchApi.java
+++ src/test/java/_base/api_helpers/buyer/BuyerSearchApi.java
@@ -7,81 +7,107 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import com.typesafe.config.Config;
 import global.Global;
 import helper.UrlHelper;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.json.Json;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BuyerSearchApi {
 
     private ApiService apiService;
     private static String buyerSiteHomeUrl = Global.config.getString("homepage.home_url");
-
-    private static String catalogUrl = Global.config.getString("homepage.home_url");
-    private static final String SELLER_URL_SUFFIX = "/?from=wangpu&q=All-Products&service=COD&ajax=true";
+    private static final String SELLER_URL_SUFFIX = "/?from=wangpu&q=All-Products&ajax=true";
     private static final String CATALOG_URL_PREFIX = "catalog/?ajax=true&from=input&service=COD&q=";
 
     public BuyerSearchApi() {
         this.apiService = new ApiService();
     }
 
-
-    public String getCodProductFromCatalog(String searchKeyword){
+    public String getNormaQnaPdpFromCatalog(String searchKeyword){
         String apiUrl = buyerSiteHomeUrl+CATALOG_URL_PREFIX+searchKeyword;
-        String productUrl = filterOneRandomProduct(apiUrl);
-        if(!productUrl.equalsIgnoreCase("")){
-            return productUrl;
-        }
-        throw new RuntimeException("Can not search product");
+        List<String> productsListUrls = getListProductUrls(apiUrl);
+        String productUrl = getQnaProduct(productsListUrls);
+        if(!productUrl.equalsIgnoreCase("")){ return productUrl;}
+        throw new RuntimeException("Can not search QNA product");
     }
 
-    @SuppressWarnings("unchecked")
-    public String getRandomCodProductFromRandomSeller(){
-        List<Object> listTestSellers  = Global.config.getList("seller.test_seller").unwrapped();
-        Collections.shuffle(listTestSellers);
-        for (Object o: listTestSellers){
-            Map<String,String> sellerMap = (Map<String, String>) o;
-            String url = getRabdomCodProductFromSpecificSeller(sellerMap.get("partial_shop_url"));
-            if(!url.equalsIgnoreCase("")) return url;
+    public String getNormaPdpFromCatalog(String searchKeyword){
+        String apiUrl = buyerSiteHomeUrl+CATALOG_URL_PREFIX+searchKeyword;
+        List<String> productsListUrls = getListProductUrls(apiUrl);
+        String productUrl = getNormalProduct(productsListUrls);
+        if(!productUrl.equalsIgnoreCase("")){ return productUrl;}
+        throw new RuntimeException("Can not search Normal product from Catalog");
+    }
+
+
+    public String getCodPdpOfTestSellers(){List<String> sellerUrls = getListTestSellerUrls();
+        if(sellerUrls==null) throw new RuntimeException("Can not search COD product");
+        for(String url: sellerUrls){
+            List<String> productsListUrls = getListProductUrls(url);
+            String productUrl = getCodProduct(productsListUrls);
+            if(!productUrl.equalsIgnoreCase("")) return productUrl;
         }
         throw new RuntimeException("Can not search COD product");
     }
 
-    private String getRabdomCodProductFromSpecificSeller(String sellerPartialUrl){
-        String apiUrl = buyerSiteHomeUrl+sellerPartialUrl+SELLER_URL_SUFFIX;
-        String productUrl = filterOneRandomProduct(apiUrl);
-        if(!productUrl.equalsIgnoreCase("")){
-            Logger.getLogger(this.getClass()).info("Product url: "+productUrl);
-            Logger.getLogger(this.getClass()).info("Seller url: "+ apiUrl);
-            return productUrl;
+    public String getQnaPdpOfTestSellers(){List<String> sellerUrls = getListTestSellerUrls();
+        if(sellerUrls==null) throw new RuntimeException("Can not search COD product");
+        for(String url: sellerUrls){
+            List<String> productsListUrls = getListProductUrls(url);
+            String productUrl = getCodProduct(productsListUrls);
+            if(!productUrl.equalsIgnoreCase("")) return productUrl;
         }
-        return "";
+        throw new RuntimeException("Can not search COD product");
     }
 
 
+    @SuppressWarnings("unchecked")
+    private List<String> getListTestSellerUrls(){
+        List<Object> listTestSellers  = Global.config.getList("seller.test_seller").unwrapped();
+        Collections.shuffle(listTestSellers);
+        return listTestSellers.stream().map(seller -> buyerSiteHomeUrl+((Map<String, String>)seller).get("partial_shop_url")+SELLER_URL_SUFFIX).collect(Collectors.toList());
+    }
 
-    private String filterOneRandomProduct(String apiUrl) {
-        try{
-            if(!apiService.isUrlValid(apiUrl)) return "";
-            JsonObject json =  apiService.get(apiUrl);
+    private String getCodProduct(List<String> productListUrls){
+        if(productListUrls==null) return "";
+        return productListUrls.stream()
+                .filter(productUrl -> apiService.isUrlValid(productUrl) && new PdpApi(productUrl).isCodPdp()).findAny().orElse("");
+    }
+
+    private String getQnaProduct(List<String> productListUrls){
+        if(productListUrls==null) return "";
+        return productListUrls.stream()
+                .filter(productUrl -> apiService.isUrlValid(productUrl) && new PdpApi(productUrl).isQnaPdp()).findAny().orElse("");
+    }
+
+    private String getNormalProduct(List<String> productListUrls){
+        if(productListUrls==null) return "";
+        return productListUrls.stream()
+                .filter(productUrl -> apiService.isUrlValid(productUrl)).findAny().orElse("");
+    }
+
+
+    private List<String> getListProductUrls(String apiUrl) {
+        try {
+            if (!apiService.isUrlValid(apiUrl)) return null;
+            JsonObject json = apiService.get(apiUrl);
             JsonArray productArray = json.getAsJsonObject("mods").getAsJsonArray("listItems");
-            if (productArray.size()==0) return "";
+            if (productArray.size() == 0) return null;
             Type listType = new TypeToken<List<JsonElement>>() {}.getType();
-            List<JsonElement> productList = new Gson().fromJson(productArray,listType);
+            List<JsonElement> productList = new Gson().fromJson(productArray, listType);
             Collections.shuffle(productList);
-            for(JsonElement o: productList){
-                String productUrl = formatProductUrl(o.getAsJsonObject().get("productUrl").getAsString());
-                if(apiService.isUrlValid(productUrl)) return productUrl;
-            }
-            return "";
-        }catch (IOException ex){
-            throw new RuntimeException("Can not search cod product using this url: "+apiUrl+" Error: "+ex.getMessage());
+            return productList.stream().map(product -> formatProductUrl(product.getAsJsonObject().get("productUrl").getAsString())).collect(Collectors.toList());
+        } catch (IOException ex) {
+            throw new RuntimeException("Can not search cod product using this url: " + apiUrl + " Error: " + ex.getMessage());
         }
-
     }
 
     private String formatProductUrl(String unformattedUrl) {
